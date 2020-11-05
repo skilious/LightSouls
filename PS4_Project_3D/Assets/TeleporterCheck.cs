d11213527c177ac786a45be7453e9c6c0aa7cce4
Tarek Tabet <t.tabet@outlook.com>
2020-11-05 02:23:45 +0100

    RayTo Ground - Inheritance Scripts
    Teleporter Check

---
+++ PS4_Project_3D/Assets/TeleporterCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterCheck : RayToGround
+{
+    public LayerMask teleporterLayer;
+
+    private void Awake()
+    {
+        rayOrigin = transform.position;
+        rayDirection = -transform.up;
+        ray = new Ray(rayOrigin, rayDirection);
+    }
+
+    private void Update()
+    {
+        RaycastDown(teleporterLayer);
+    }
+
+    protected override void RaycastDown(LayerMask layerMask)
+    {
+        base.RaycastDown(layerMask);
+
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, layerMask))
+        {
+            // Do Teleporter Stuff
+            // Text Popup: "Press X to Enter Stage"
+            raycastHit.collider.GetComponent<Renderer>().material.color = tintColor;
+        }
+    }
+}
