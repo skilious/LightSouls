99f3845a9f9bc9857012f04a153163c4b2f486ec
Skilious <onlinegamingsocial@gmail.com>
2020-11-17 13:21:48 +1100

    Computer is fucked so I decided to just do this for now.

---
+++ PS4_Project_3D/Assets/Scripts/SoulEssence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulEssence : MonoBehaviour
+{
+    private Transform player;
+    
+    [SerializeField]
+    private float speed = 0.0f;
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < 3.0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
+        }
+    }
+}
