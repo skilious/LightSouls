bb9523b473cc93e2520c7df7e50a7745aa1b2fa2
Tarek Tabet <t.tabet@outlook.com>
2020-11-20 00:15:09 +0100

    Finished SoulWall
    Left two soul essences in scene but if you swap them out for the basic enemy that drops a soul essence it works the same. left it like this for basic testing. AKA kill one and the door opens.
    Next thing: the text popups or the other stages?

--- PS4_Project_3D/Assets/Scripts/SoulEssence.cs
+++ PS4_Project_3D/Assets/Scripts/SoulEssence.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoulEssence : MonoBehaviour
 {
+    // Event Shoutouts
+    public static event Action<SoulEssence> OnSoulAbsorbed = delegate { };
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, soulDoor.transform.position, Time.deltaTime * speed);
+    }
     private GameObject soulDoor;
     
     [SerializeField]
     private float speed = 0.0f;
-    
 
     void Start()
     {
         soulDoor = GameObject.Find("SoulAbsorbPoint");
-    }
-
-    void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, soulDoor.transform.position, Time.deltaTime * speed);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,8 +27,17 @@ public class SoulEssence : MonoBehaviour
         if(other.GetComponent<BoxCollider>())
         {
             Debug.Log("Soul Absorbed");
+
+            // Invoke OnSoulAbsorbed Event
+            OnSoulAbsorbed(this);
+
             Character_Status.soulEssence++; //Increments soulEssence.
             Destroy(gameObject); //Destroy this gameObject.
         }
+    }
+
+    private void OnDestroy()
+    {
+        
     }
 }
--- PS4_Project_3D/Assets/SoulWallHandler.cs
+++ PS4_Project_3D/Assets/SoulWallHandler.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoulWallHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int soulsAbsorbedCount;
+
+    [SerializeField]
+    private List<GameObject> SoulFire;
+
+    private BoxCollider soulWallBoxCollider;
+    private ParticleSystem soulWallParticleSystem;
+
+    private void Start()
     {
-        
+        soulWallBoxCollider = GetComponentInParent<BoxCollider>();
+        soulWallParticleSystem = GetComponentInParent<ParticleSystem>();
+        soulsAbsorbedCount = 0;
+        SoulEssence.OnSoulAbsorbed += SoulWallHandler_OnSoulAbsorbed; //Removed TeleporterCheck as its already inherited from the "TeleporterCheck" class...
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SoulWallHandler_OnSoulAbsorbed(SoulEssence soulEssence)
     {
-        
+        soulsAbsorbedCount++;
+        Debug.Log(soulsAbsorbedCount);
+        if (soulsAbsorbedCount == 1)
+        {
+            SoulFire[0].SetActive(true);
+        }
+        else if (soulsAbsorbedCount == 2)
+        {
+            SoulFire[1].SetActive(true);
+        }
+        else if (soulsAbsorbedCount == 3)
+        {
+            SoulFire[2].SetActive(true);
+            soulWallBoxCollider.enabled = false;
+            var psMain = soulWallParticleSystem.main;
+            psMain.loop = false;
+        }
     }
+
+    // Do Teleporter Stuff
+    // OnTeleporter.Invoke(this, System.EventArgs.Empty);
 }
