53a5957d330fe94fb8ae9e911662d510bca10d2c
Tarek Tabet <t.tabet@outlook.com>
2020-11-23 00:40:39 +0100

    Completed Dialog Animator Stuff.
    Setting the Text for each popup will done tomorrow. This isn't priority right now. worst case scenario we set em up in the editor for each scene. One popup per scene. can quick fix by disabling "used" dialog boxes to avoid overlaps.
    Will work on Traps tomorrow.

--- PS4_Project_3D/Assets/DialogController.cs
+++ PS4_Project_3D/Assets/DialogController.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogController : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas dialogCanvas;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private TMP_Text text;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        animator = dialogCanvas.GetComponentInChildren<Animator>();
+        text = dialogCanvas.GetComponentInChildren<TMP_Text>();
+        animator.SetBool("showDialog", false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(animator.GetBool("showDialog"))
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                animator.SetBool("showDialog", false);
+            }
+        }
+    }
+
+    protected void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("showDialog", true);
+        }
+    }
+    protected void OnTriggerExit(Collider collision)
+    {
+        animator.SetBool("showDialog", false);
+    }
+
+    protected virtual void SetDialogText()
+    {
+
     }
 }
