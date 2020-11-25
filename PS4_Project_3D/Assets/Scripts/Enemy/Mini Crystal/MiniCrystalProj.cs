b1baad697a98c56959c14928791b4a658119fe3b
Skilious <onlinegamingsocial@gmail.com>
2020-11-26 00:26:59 +1100

    Mini Crystal added to the prefab with all their own projectile script. They just need a replacement projectile which I'll work on tomorrow. Main character added without animations. It looks extremely weird ngl.

---
+++ PS4_Project_3D/Assets/Scripts/Enemy/Mini Crystal/MiniCrystalProj.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCrystalProj : EnemyAI
+{
+    public void StartAttack()
+    {
+        InvokeRepeating("Attack", attackTimer, repeatTimer);
+    }
+
+    public void StopAttack()
+    {
+        CancelInvoke("Attack");
+    }
+
+    private void Attack()
+    {
+        GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+        obj.SetActive(true);
+        obj.transform.position = transform.position + transform.forward;
+        obj.transform.rotation = Quaternion.identity;
+        Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+        cloneRB.AddForce(transform.forward * 250.0f, ForceMode.Acceleration);
+    }
+}
