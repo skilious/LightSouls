e9401b3867c7c333e679715750a2ce5b0ff5a7a6
Tarek Tabet <t.tabet@outlook.com>
2020-11-25 01:06:10 +0100

    Set up for trap
    Watched too many videos to se how to implement cleanly. my head exploded and I did nothing in the end... but tomorrow is another day lol

---
+++ PS4_Project_3D/Assets/TrapHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapHandler : MonoBehaviour
+{
+    [SerializeField] private float _countdownTime = 1f;
+
+    private float _explodeTime;
+
+}
