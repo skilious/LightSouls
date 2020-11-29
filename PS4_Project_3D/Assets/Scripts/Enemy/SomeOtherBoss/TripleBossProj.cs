88ad3c2a0bfcc818c9c566da96010878852c9339
Skilious <onlinegamingsocial@gmail.com>
2020-11-29 17:44:40 +1100

    Boss Enemy fixed with the projectiles shooting at their own respectable locations. Two added abilities to the boss.

--- PS4_Project_3D/Assets/Scripts/Enemy/SomeOtherBoss/TripleBossProj.cs
+++ PS4_Project_3D/Assets/Scripts/Enemy/SomeOtherBoss/TripleBossProj.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class TripleBossProj : EnemyAI
 {
     /*TODO: 
-    Create three projectiles at three different locations
-    Shoot in their own projectiles at their own locations
-    Aim directly towards the player's last location.
-    Adjustable firerates
+    Create three projectiles at three different locations DONE
+    Shoot in their own projectiles at their own locations DONE
+    Aim directly towards the player's last location. DONE
+    Adjustable firerates 
 
-    OBJ_Pooling support - Reference three GameObjects as Projectiles.
+    OBJ_Pooling support - Reference three GameObjects as Projectiles. DONE
+
+    Enumerator abilities
     */
-    [SerializeField] private Vector3[] projectilePosition;
+    private enum Abilities
+    {
+        basic,
+        worldspread,
+        frontalspread
+    };
 
+    [SerializeField] private Vector3[] projectilePosition;
     [SerializeField] private Transform[] projectileSpawn;
 
+    private int pattern = 0;
+    private Abilities ability;
+    private float timer = 0.0f;
     protected override void Start()
     {
         base.Start();
@@ -33,6 +44,7 @@ public class TripleBossProj : EnemyAI
     protected override void Update()
     {
         base.Update();
+        timer += Time.deltaTime;
     }
 
     private void Attack()
@@ -42,46 +54,111 @@ public class TripleBossProj : EnemyAI
 
     private IEnumerator Projectile()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.2f);
-
-        for (int i = 0; i < projectileSpawn.Length; i++)
+        WaitForSeconds wait = new WaitForSeconds(0.3f);
+        if(timer >= 5.0f)
         {
-            projectilePosition[i] = projectileSpawn[i].position;
-            switch (i)
-            {
-                case 0:
-                    {
-                        GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
-                        obj.SetActive(true);
-                        obj.transform.position = projectilePosition[i];
-                        obj.transform.LookAt(player.transform.position);
-                        Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
-                        cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
-                        break;
-                    }
-                case 1:
-                    {
-                        GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
-                        obj.SetActive(true);
-                        obj.transform.position = projectilePosition[i];
-                        obj.transform.LookAt(player.transform.position);
-                        Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
-                        cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
-                        break;
-                    }
-                case 2:
-                    {
-                        GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
-                        obj.SetActive(true);
-                        obj.transform.position = projectilePosition[i];
-                        obj.transform.LookAt(player.transform.position);
-                        Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
-                        cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
-                        break;
-                    }
-            }
-            yield return wait;
+            int abilitySwap = Random.Range(0, 3);
+            ability = (Abilities)abilitySwap;
+            timer = 0.0f;
         }
-
+        switch (ability)
+        {
+            case Abilities.basic:
+                {
+                    for (int i = 0; i < projectileSpawn.Length; i++)
+                    {
+                        int randomThrow = Random.Range(0, 3);
+                        projectilePosition[i] = projectileSpawn[randomThrow].position;
+                        switch (randomThrow)
+                        {
+                            case 0:
+                                {
+                                    GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                                    obj.SetActive(true);
+                                    obj.transform.position = projectilePosition[i];
+                                    obj.transform.LookAt(player.transform.position);
+                                    Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                                    cloneRB.AddForce(obj.transform.forward * 500.0f, ForceMode.Acceleration);
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                                    obj.SetActive(true);
+                                    obj.transform.position = projectilePosition[i];
+                                    obj.transform.LookAt(player.transform.position);
+                                    Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                                    cloneRB.AddForce(obj.transform.forward * 750.0f, ForceMode.Acceleration);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                                    obj.SetActive(true);
+                                    obj.transform.position = projectilePosition[i];
+                                    obj.transform.LookAt(player.transform.position);
+                                    Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                                    cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
+                                    break;
+                                }
+                        }
+                        yield return wait;
+                    }
+                    yield return null;
+                    break;
+                }
+            case Abilities.frontalspread:
+                {
+                    float spread = -90.0f;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                        obj.SetActive(true);
+                        obj.transform.position = transform.position + transform.up * 0.5f;
+                        obj.transform.rotation = transform.rotation;
+                        obj.transform.Rotate(0, spread, 0);
+                        Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                        cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
+                        spread += 18.0f;
+                    }
+                    break;
+                }
+            case Abilities.worldspread:
+                {
+                    if(pattern == 0)
+                    {
+                        float spread = -360.0f;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                            obj.SetActive(true);
+                            obj.transform.position = transform.position + transform.up * 0.5f; ;
+                            obj.transform.rotation = Quaternion.identity;
+                            obj.transform.Rotate(0, spread, 0);
+                            Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                            cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
+                            spread += 36.0f;
+                        }
+                        pattern++;
+                    }
+                    else if(pattern == 1)
+                    {
+                        float spread = 15.0f;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            GameObject obj = Object_Pooling.SharedInstance.GetPooledObject("EnemyProjectile");
+                            obj.SetActive(true);
+                            obj.transform.position = transform.position + transform.up * 0.5f; ;
+                            obj.transform.rotation = Quaternion.identity;
+                            obj.transform.Rotate(0, spread, 0);
+                            Rigidbody cloneRB = obj.GetComponent<Rigidbody>();
+                            cloneRB.AddForce(obj.transform.forward * 350.0f, ForceMode.Acceleration);
+                            spread += 36.0f;
+                        }
+                        pattern--;
+                    }
+                    break;
+                }
+        }
     }
 }
--- PS4_Project_3D/Assets/TripleBoss_Attack.cs
+++ PS4_Project_3D/Assets/TripleBoss_Attack.cs
@@ -9,18 +9,18 @@ public class TripleBoss_Attack : NPCBase
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
         NPC.GetComponent<TripleBossProj>().StartAttack();
+        agent.isStopped = true;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Quaternion lookTarget = Quaternion.LookRotation(target.transform.position - NPC.transform.position);
-        NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, lookTarget, rotSpeed * Time.deltaTime);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         NPC.GetComponent<TripleBossProj>().StopAttack();
+        agent.isStopped = false;
     }
 }
